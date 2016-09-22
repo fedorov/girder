b9dd9d5a77d9cd24656d16ac986fc96cf2a1aee4
Chris Harris <chris.harris@kitware.com>
2016-09-22 17:18:19 -0400

    Handle jobs without a type defined

--- plugins/jobs/web_client/js/views/JobListWidget.js
+++ plugins/jobs/web_client/js/views/JobListWidget.js
@@ -81,7 +81,7 @@ girder.views.jobs_JobListWidget = girder.View.extend({
         }));
 
         types = _.uniq(this.collection.toArray().map(function (job) {
-            return job.attributes.type;
+            return job.attributes.type ? job.attributes.type : '';
         }));
 
         this._updateFilter(this.typeFilter, types);
@@ -131,12 +131,13 @@ girder.views.jobs_JobListWidget = girder.View.extend({
     },
     _filterJobs: function (jobs) {
         var filterJobs = [];
-
-        // Include all jobs that have matching state or type field.
+        // Include all jobs that match the type and status filters. Jobs that
+        // have an undefined type are mapped to '', this is added as a filter
+        // option for the user to select.
         filterJobs = this.collection.filter(_.bind(function (job) {
-            return ((_.isEmpty(this.typeFilter) || _.isUndefined(job.attributes.type) ||
-                        this.typeFilter[job.attributes.type]) &&
-                    (_.isEmpty(this.statusFilter) || _.isUndefined(job.attributes.status) ||
+            return ((_.isEmpty(this.typeFilter) ||
+                        this.typeFilter[job.attributes.type ? job.attributes.type : '']) &&
+                    (_.isEmpty(this.statusFilter) ||
                         this.statusFilter[girder.jobs_JobStatus.text(job.attributes.status)]));
         }, this));
 
